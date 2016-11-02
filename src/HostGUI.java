24504de243a791952b28f2ff49e0a3a0341d11e1
Kaye Suarez <suarezka@mail.gvsu.edu>
2016-11-02 17:53:20 -0400

    GUI Improvements

--- src/HostGUI.java
+++ src/HostGUI.java
@@ -1,33 +1,112 @@
 import java.awt.*;
 
-import javax.swing.BorderFactory;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 
-public class HostGUI extends Frame { 
+public class HostGUI extends Frame {
+    
+    private JPanel top;
+    private JPanel center;
+    private JPanel bottom;
+    private JLabel sHostname;
+    private JLabel port;
+    private JLabel username;
+    private JLabel hostname;
+    private JLabel speed;
+    private JLabel keyword;
+    private JLabel command;
+    private JButton connect;
+    private JButton search;
+    private JButton go;
+    private JTextField sHostnameInput;
+    private JTextField portInput;
+    private JTextField usernameInput;
+    private JTextField hostnameInput;
+    private JTextField keywordInput;
+    private JTextArea ftpCommands;
+    private JComboBox selectSpeed;
+    
+    private String[] speeds = { "Ethernet", "Wifi", "T1", "T3" };
+    
+    
 	
-	public ChatFrame (String title) {
+	public HostGUI (String title) {
 		super (title);
-		
-		JPanel top  = new JPanel();
-		frame.getContentPane().add(top, BorderLayout.NORTH);
+        
+        //setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+
+        //Connect Panel - Top
+		top  = new JPanel();
+		add(top, BorderLayout.NORTH);
 		top.setBorder(BorderFactory.createTitledBorder("Connect"));
-		
-		JPanel center = new JPanel();
-		frame.getContentPane().add(center, BorderLayout.CENTER);
+        top.setPreferredSize(new Dimension(700, 140));
+        
+        FlowLayout topPanel = new FlowLayout();
+        top.setLayout(topPanel);
+        
+        //First Row of Panel
+        sHostname = new JLabel("Server Hostname:");
+        sHostnameInput = new JTextField(20);
+        //sHostnameInput.addActionListener();
+        
+        port = new JLabel("Port:");
+        portInput = new JTextField(10);
+        connect = new JButton("Connect");
+        
+        top.add(sHostname);
+        top.add(sHostnameInput);
+        top.add(port);
+        top.add(portInput);
+        top.add(connect);
+        
+        //Second Row of Panel
+        username = new JLabel("Username:");
+        usernameInput = new JTextField(20);
+        hostname = new JLabel("Hostname:");
+        sHostnameInput = new JTextField(20);
+        speed = new JLabel("Speed:");
+        selectSpeed = new JComboBox(speeds);
+        
+        top.add(username);
+        top.add(usernameInput);
+        top.add(hostname);
+        top.add(sHostnameInput);
+        top.add(speed);
+        top.add(selectSpeed);
+
+		//Search Panel - Center
+		center = new JPanel();
+		add(center, BorderLayout.CENTER);
 		center.setBorder(BorderFactory.createTitledBorder("Search"));
+        keyword = new JLabel("Keyword:");
+        keywordInput = new JTextField(20);
+        search = new JButton("Search");
+        
+        center.add(keyword);
+        center.add(keywordInput);
+        center.add(search);
 		
-		JPanel bottom = new JPanel();
-		frame.getContentPane().add(bottom, BorderLayout.SOUTH);
+        //FTP Panel - Bottom
+		bottom = new JPanel();
+		add(bottom, BorderLayout.SOUTH);
 		bottom.setBorder(BorderFactory.createTitledBorder("FTP"));
+        command = new JLabel("Enter Command:");
+        go = new JButton("Go");
+        ftpCommands = new JTextArea("FTP Commands...Go Here");
+        ftpCommands.setEditable(false);
+        
+        bottom.add(command);
+        bottom.add(go);
+        bottom.add(ftpCommands);
+        
 		
-		pack (); 
-	    show (); 
-	    input.requestFocus ();
+        pack ();
+        show ();
+        //input.requestFocus ();
 	}
 
 	public static void main(String[] args) {
-		new HostGUI("NAPSTER FRIENDS");	
+        new HostGUI("NAPSTER FRIENDS");
 	}
 }
